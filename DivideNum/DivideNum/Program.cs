d8c4154300429ad60f744818e4eb62754a8c685c
mnava18 <mnava18@sbcglobal.net>
2017-06-08 00:02:34 -0700

    unit testing
    UT practice

---
+++ DivideNum/DivideNum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivideNum
+{
+   public class Program
+    {
+        public static int doStuff(int num1, int num2)
+        {
+            int result = num1 / num2;
+            return result;
+
+
+        }
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ DivideNum/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using DivideNum;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //arrange
+            int a = 10;
+            int b = 2;
+            int expected = 5;
+
+            //act
+          int actual=  DivideNum.Program.doStuff(a, b);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
